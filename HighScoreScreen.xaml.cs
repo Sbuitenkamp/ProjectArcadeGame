ffa505e09d943c4a4b4479c11b7745802fd648bb
Khalid <48758660+khalido1990@users.noreply.github.com>
2021-10-25 12:54:32 +0200

    Connected to database and shows highscores in highscore screen

--- HighScoreScreen.xaml.cs
+++ HighScoreScreen.xaml.cs
@@ -1,4 +1,9 @@
-﻿using System.Windows;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using Tron_Mario.Models;
 
 namespace Tron_Mario
 {
@@ -7,10 +12,44 @@ namespace Tron_Mario
     /// </summary>
     public partial class HighScoreScreen : Window
     {
+        private DatabaseHandler DatabaseHandler = new DatabaseHandler();
         public HighScoreScreen()
         {
             InitializeComponent();
+            DatabaseHandler.GetHighScoresSinglePlayer();
+            DatabaseHandler.GetHighScoresMultiPlayer();
+            CreateLabels();
         }
+        
+        private void CreateLabels()
+        {
+            SinglePlayerHighScoresPanel.Children.Clear();
+            MultiPlayerHighScoresPanel.Children.Clear();
+            var sortedHighScoresSinglePlayer = from score in DatabaseHandler.HighScoresSinglePlayer select score;
+            var sortedHighScoresMultiPlayer = from score in DatabaseHandler.HighScoresMultiPlayer select score;
+            foreach (var highScoreSinglePlayer in sortedHighScoresSinglePlayer)
+            {
+                var label = new Label
+                {
+                    Content = highScoreSinglePlayer.Key + " scored " + highScoreSinglePlayer.Value,
+                    Background = Brushes.White,
+                    HorizontalAlignment = HorizontalAlignment.Center
+                };
+                SinglePlayerHighScoresPanel.Children.Add(label);
+            }
+            foreach (var highScoreMultiPlayer in sortedHighScoresMultiPlayer)
+            {
+                var labels = new Label
+                {
+                    Content = highScoreMultiPlayer.Key + " scored " + highScoreMultiPlayer.Value,
+                    Background = Brushes.White,
+                    HorizontalAlignment = HorizontalAlignment.Center
+                };
+                MultiPlayerHighScoresPanel.Children.Add(labels);
+
+            }
+        }
+
 
         private void MainMenu(object sender, RoutedEventArgs e)
         {
